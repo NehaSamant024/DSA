e86ce833b4dfc5b48512096b70fc2d8bc2efc66e
NehaSamant024 <91238040+NehaSamant024@users.noreply.github.com>
2022-04-05 23:25:00 +0530

    Create input_res.double_ended_circularqueue.c

---
+++ input_res.double_ended_circularqueue.c
@@ -0,0 +1,258 @@
+//input restricted double ended circular queue
+#include<stdio.h>
+#include<stdlib.h>
+
+int isEmpty(int q[],int n, int*front, int*rear);
+
+int isFull(int q[],int n, int*front, int*rear);
+
+void insert_rear(int q[],int n, int*front, int*rear, int value);
+
+int remove_front(int q[],int n, int*front, int*rear);
+
+int remove_rear(int q[],int n, int*front, int*rear);
+
+int get_front(int q[],int n, int*front, int*rear);
+
+int get_rear(int q[],int n, int*front, int*rear);
+
+void display(int q[],int n, int*front, int*rear);
+
+int main()
+{
+    int n;
+    printf("Enter max size of queue:");
+    scanf("%d",&n);
+    int queue[n];
+    int front=-1,rear=-1;
+    while(1)
+    {
+        char *s="OPERATION PERFORMED SUCCESSFULLY";
+        printf("\n\n*************************************************************************************\n\n");
+        printf("YOU CAN PERFORM FOLLOWING OPERATIONS...........\n");
+        printf("\n___________MENU___________\n");
+        printf("\n1.INSERT AT REAR\n");
+        printf("2.REMOVE FROM FRONT\n");
+        printf("3.REMOVE FROM REAR\n");
+        printf("4.GET FRONT\n");
+        printf("5.GET REAR\n");
+        printf("6.IS EMPTY ?\n");
+        printf("7.IS FULL ?\n");
+        printf("8.DISPLAY\n");
+        printf("9.EXIT\n");
+        printf("\nEnter your choice::");
+        int c;
+        scanf("%d",&c);
+        switch(c)
+        {
+            
+            case 1:
+            {
+                int value;
+                printf("\nEnter value to be inserted at rear:");
+                scanf("%d",&value);
+                insert_rear(queue,n,&front,&rear,value);
+                break;
+            }
+            case 2:
+            {
+                int value;
+                value=remove_front(queue,n,&front,&rear);
+                if(value!=-1)
+                {
+                    printf("\nValue %d removed from front",value);
+                }
+                break;
+            }
+            case 3:
+            {
+                int value;
+                value=remove_rear(queue,n,&front,&rear);
+                if(value!=-1)
+                {
+                    printf("\nValue %d removed from rear",value);
+                }
+                break;
+            }
+            case 4:
+            {
+                int value;
+                value=get_front(queue,n,&front,&rear);
+                if(value!=-1)
+                {
+                    printf("\nValue at front: %d",value);
+                }
+                break;
+            }
+            case 5:
+            {
+                int value;
+                value=get_rear(queue,n,&front,&rear);
+                if(value!=-1)
+                {
+                    printf("\nValue at rear: %d",value);
+                }
+                break;
+            }
+            case 6:
+            {
+                if(isEmpty(queue,n,&front,&rear))
+                {
+                    printf("\nQueue is empty");
+                }
+                else
+                {
+                    printf("\nQueue is not empty");
+                }
+                break;
+            }
+            case 7:
+            {
+                if(isFull(queue,n,&front,&rear))
+                {
+                    printf("\nQueue is full");
+                }
+                else
+                {
+                    printf("\nQueue is not full");
+                }
+                break;
+            }
+            case 8:
+            {
+                display(queue,n,&front,&rear);
+                break;
+            }
+            case 9:
+            {
+                printf("\n\n*************************************************************************************\n\n");
+                printf("                                   PROGRAM FINISHED                                        ");
+                printf("\n\n*************************************************************************************\n\n");
+                exit(0);
+            }
+            default: s="        INVALID CHOICE         ";
+        }
+        printf("\n\n*************************************************************************************\n\n");
+        printf("                         %s                            ",s);
+    }
+    return 0;
+}
+
+
+int isEmpty(int q[],int n, int*front, int*rear)
+{
+    if(*front==-1){return 1;}
+    return 0;
+}
+
+
+int isFull(int q[],int n, int*front, int*rear)
+{
+    if((*front==(*rear+1)%n)){return 1;}
+    return 0;
+}
+
+
+void insert_rear(int q[],int n, int*front, int*rear, int value)
+{
+    if(isFull(q,n,front,rear))
+    {
+        printf("\nQueue is Full");
+        return;
+    }
+    if(*rear==-1)
+    {
+        *front=0;
+        *rear=0;
+    }
+    else 
+    {
+        *rear=(*rear+1)%n;
+    }
+    q[*rear]=value;
+}
+
+
+int remove_front(int q[],int n, int*front, int*rear)
+{
+    if(isEmpty(q,n,front,rear))
+    {
+        printf("\nQueue is Empty");
+        return -1;
+    }
+    int value=q[*front];
+    if(*front==*rear)
+    {
+        *front=-1;
+        *rear=-1;
+    }
+    else 
+    {
+        *front=(*front+1)%n;
+    }
+    return value;
+}
+
+
+int remove_rear(int q[],int n, int*front, int*rear)
+{
+    if(isEmpty(q,n,front,rear))
+    {
+        printf("\nQueue is Empty");
+        return -1;
+    }
+    int value=q[*rear];
+    if(*front==*rear)
+    {
+        *front=-1;
+        *rear=-1;
+    }
+    else if(*rear==0)
+    {
+        *rear=n-1;
+    }
+    else 
+    {
+        (*rear)--;
+    }
+    return value;
+}
+
+
+int get_front(int q[],int n, int*front, int*rear)
+{
+    if(isEmpty(q,n,front,rear))
+    {
+        printf("\nQueue is empty, no element at front");
+        return -1;
+    }
+    return q[*front];
+}
+
+
+int get_rear(int q[],int n, int*front, int*rear)
+{
+    if(isEmpty(q,n,front,rear))
+    {
+        printf("\nQueue is empty, no element at rear");
+        return -1;
+    }
+    return q[*rear];
+}
+
+
+void display(int q[],int n, int*front, int*rear)
+{
+    if(isEmpty(q,n,front,rear))
+    {
+        printf("\nQueue is Empty");
+        return;
+    }
+    printf("Queue:: ");
+    int i;
+    for(i=*front;i!=(*rear);i=(i+1)%n)
+    {
+        printf("%d ",q[i]);
+    }
+    printf("%d ",q[i]);
+}
