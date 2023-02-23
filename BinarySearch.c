5d5d61694a799bf2dd14260d5286994d2cf2ac23
Neha Samant <91238040+NehaSamant024@users.noreply.github.com>
2023-02-23 19:03:32 +0530

    Create BinarySearch.c

---
+++ BinarySearch.c
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int binarySearch(int arr[],int size,int key)
+{
+    int low=0;
+    int high=size-1;
+    int mid=(low+high)/2;
+    while(low<=high)
+    {
+        if(arr[mid]==key)
+        {
+            return mid;
+        }
+        if(arr[mid]<key)
+        {
+            low=mid+1;
+        }
+        else
+        {
+            high=mid-1;
+        }
+        mid=(low+high)/2;
+    }return -1;
+}
+int main(){
+    int key;
+int arr[]={2,3,4,5,7,8,9,10};
+//////cout<<"enter key\n";
+//cin>>key;
+int index=binarySearch(arr,8,10);
+cout<<"key found at the index"<<index;
+return 0;
+}
